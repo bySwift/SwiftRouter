09ffff637d2e50b7270e08b1ad3160ea4be4aded
lecco <leccoer@gmail.com>
2018-11-06 17:27:48 +0800

    Initial Commit

---
+++ SwiftRouterDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftRouterDemo
+//
+//  Created by lecco on 2018/11/6.
+//  Copyright © 2018 lecco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
